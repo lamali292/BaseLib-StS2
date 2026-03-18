dd98d24615a219c2fde84541643028e297171e8b
Alchyr <isithalchyr@gmail.com>
2026-03-17 20:23:01 -0700

    fast mode float util

---
+++ Extensions/FloatExtensions.cs
@@ -0,0 +1,17 @@
+﻿using MegaCrit.Sts2.Core.Saves;
+using MegaCrit.Sts2.Core.Settings;
+
+namespace BaseLib.Extensions;
+
+public static class FloatExtensions
+{
+    public static float OrFast(this float time)
+    {
+        return SaveManager.Instance.PrefsSave.FastMode switch
+        {
+            FastModeType.Instant => 0.01f,
+            FastModeType.Fast => time * 0.3f,
+            _ => time
+        };
+    }
+}
